13fd3bf801650a5960ced1d9277746d84663fa6e
Felipe Morales <hel.sheep@gmail.com>
2010-05-19 15:05:40 -0400

    Added a script to build a deb package (requires py2deb), and a changelog.

---
+++ make_deb.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from py2deb import Py2deb
+
+version = "0.1"
+changelog = open("changelog.txt", "r").read()
+
+p = Py2deb("indimpc")
+p.author = "Felipe Morales"
+p.mail = "hel.sheep@gmail.com"
+p.description = "Minimalistic MPD client with support for appindicator, multimedia keys and notifications."
+p.url = "http://github.com/fmoralesc/indimpc"
+p.depends = "python-dbus, python-notify, python-appindicator, python-mpd"
+p.license = "gpl"
+p.section = "sound"
+p.arch = "all"
+
+p["/usr/bin"] = ["indimpc.py|indimpc"]
+
+p.generate(version, changelog)
